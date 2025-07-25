2b76230f92c65ad8c0e3e0d00be41eaf50d86cd2
phuong456 <phuongnlh.29@gmail.com>
2025-07-25 09:21:35 +0700

    Change socket noti

--- src/app.js
+++ src/app.js
@@ -13,15 +13,6 @@ const http = require("http");
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"], // Allow Vite dev server
-    methods: ["GET", "POST"],
-    credentials: true, // Allow cookies
-  },
-});
-
-require("./socket")(io);
 
 // CORS configuration for HTTP requests
 app.use(
