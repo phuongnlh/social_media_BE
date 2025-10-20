6679b0754a6e3ddc9de5416291f7a89c660f3f0d
phuong456 <phuongnlh.29@gmail.com>
2025-10-21 00:05:34 +0700

    Update payment.controller.js

--- src/controllers/payment.controller.js
+++ src/controllers/payment.controller.js
@@ -190,6 +190,7 @@ const createMoMoSession = async (req, res, ads, payment, amount) => {
     });
     // Make request to MoMo
     const momoResponse = await makeMoMoRequest(requestBody);
+    console.log("Ipn Url:", ipnUrl);
 
     if (momoResponse.resultCode === 0) {
       // Update payment record with MoMo info
