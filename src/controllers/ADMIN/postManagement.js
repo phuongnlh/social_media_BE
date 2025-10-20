b31a3f3ddd111adfa9f63406f25acc731c0b97f8
phuong456 <phuongnlh.29@gmail.com>
2025-10-20 12:45:12 +0700

    Update postManagement.js

--- src/controllers/ADMIN/postManagement.js
+++ src/controllers/ADMIN/postManagement.js
@@ -229,8 +229,8 @@ const getAllPosts = async (req, res) => {
     ];
 
     // Handle media filter in aggregation if needed
-    if (hasMedia !== "") {
-      const mediaCondition = hasMedia === "true" ? { $ne: [] } : { $eq: [] };
+    if (hasMedia !== "" && hasMedia !== undefined && hasMedia !== null) {
+      const mediaCondition = hasMedia ? { $ne: [] } : { $eq: [] };
 
       // Add media lookup before the match stage
       aggregationPipeline.splice(-3, 0, {
