ad96b6b8908e870a60fe2c3d611b76e473d0a19a
phuong456 <phuongnlh.29@gmail.com>
2025-11-05 17:49:37 +0700

    Add firebase-admin and update Firebase config
    Added firebase-admin to dependencies and updated .gitignore to include admin-google-services.json. Modified Firebase configuration to support admin SDK integration.

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 
 // Đọc file JSON theo cách CommonJS
-const serviceAccountPath = path.join(__dirname, "../../google-services.json");
+const serviceAccountPath = path.join(__dirname, "../../admin-google-services.json");
 const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 if (!admin.apps.length) {
   admin.initializeApp({
