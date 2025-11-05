53c7f1067b32d58518446f7ec3b9ee52debe8bb2
phuong456 <phuongnlh.29@gmail.com>
2025-11-05 17:42:40 +0700

    Update firebase.js

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,10 @@
 const admin = require("firebase-admin");
-import serviceAccount from "../../google-services.json" assert { type: "json" };
+const path = require("path");
+const fs = require("fs");
+
+// Đọc file JSON theo cách CommonJS
+const serviceAccountPath = path.join(__dirname, "../../google-services.json");
+const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf8"));
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
